617edee33ed32cfaa33c7589caf9a07f48ccc037
elviredev <sandrine.rodriguez@hotmail.com>
2024-06-26 09:50:08 +0200

    ✨Utilisation de AuthenticatedLayout sur la homepage pour afficher la Navbar

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -11,11 +11,10 @@ const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-gray-100">
+        <div class="h-full overflow-hidden flex flex-col bg-gray-100">
             <nav class="bg-white border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="px-3 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
                         <div class="flex">
                             <!-- Logo -->
@@ -25,13 +24,6 @@ const showingNavigationDropdown = ref(false);
                                         class="block h-9 w-auto fill-current text-gray-800"
                                     />
                                 </Link>
-                            </div>
-
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
                             </div>
                         </div>
 
@@ -111,12 +103,6 @@ const showingNavigationDropdown = ref(false);
                     :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
                     class="sm:hidden"
                 >
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
                     <!-- Responsive Settings Options -->
                     <div class="pt-4 pb-1 border-t border-gray-200">
                         <div class="px-4">
@@ -144,9 +130,8 @@ const showingNavigationDropdown = ref(false);
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main class="flex-1 overflow-hidden">
                 <slot />
             </main>
         </div>
-    </div>
 </template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({
 
@@ -15,7 +16,8 @@ defineProps({
 <template>
     <Head title="Social Media Website" />
 
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
         <!-- Partie gauche - Groupes -->
         <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
             <GroupList />
@@ -32,4 +34,5 @@ defineProps({
             <PostList class="flex-1" />
         </div>
     </div>
+    </AuthenticatedLayout>
 </template>
